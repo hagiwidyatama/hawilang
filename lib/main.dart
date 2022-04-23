5b258a5db379341213e0db09157f7d4d94c5214d
hagiwidyatama/hawilang.git <git@github.com>
2022-04-23 12:50:19 +0700

    bottomnav

--- lib/main.dart
+++ lib/main.dart
@@ -29,39 +29,135 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  int _bottomNavCurrentIndex = 0;
+  final List<Widget> _container = [
+    const Beranda(),
+    const Produk(),
+    const Akun()
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        body: _container[_bottomNavCurrentIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          onTap: (index) {
+            setState(() {
+              _bottomNavCurrentIndex = index;
+            });
+          },
+          currentIndex: _bottomNavCurrentIndex,
+          items: const [
+            BottomNavigationBarItem(
+              activeIcon: Icon(
+                Icons.home,
+                color: Colors.red,
+              ),
+              icon: Icon(
+                Icons.home,
+                color: Colors.grey,
+              ),
+              label: 'Beranda',
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            BottomNavigationBarItem(
+              activeIcon: Icon(
+                Icons.assignment,
+                color: Colors.red,
+              ),
+              icon: Icon(
+                Icons.assignment,
+                color: Colors.grey,
+              ),
+              label: 'Produk',
             ),
+            BottomNavigationBarItem(
+              activeIcon: Icon(
+                Icons.people,
+                color: Colors.red,
+              ),
+              icon: Icon(
+                Icons.people,
+                color: Colors.grey,
+              ),
+              label: 'Akun',
+            )
           ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
+        ));
+  }
+}
+
+class Beranda extends StatefulWidget {
+  const Beranda({Key? key}) : super(key: key);
+
+  @override
+  _BerandaState createState() => _BerandaState();
+}
+
+class _BerandaState extends State<Beranda> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+      child: Container(
+          color: Colors.white,
+          child: Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const <Widget>[
+                  Text('Beranda'),
+                ]),
+          )),
+    ));
+  }
+}
+
+class Produk extends StatefulWidget {
+  const Produk({Key? key}) : super(key: key);
+
+  @override
+  _ProdukState createState() => _ProdukState();
+}
+
+class _ProdukState extends State<Produk> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+      child: Container(
+          color: Colors.white,
+          child: Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const <Widget>[
+                  Text('Produk'),
+                ]),
+          )),
+    ));
+  }
+}
+
+class Akun extends StatefulWidget {
+  const Akun({Key? key}) : super(key: key);
+
+  @override
+  _AkunState createState() => _AkunState();
+}
+
+class _AkunState extends State<Akun> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+      child: Container(
+          color: Colors.white,
+          child: Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const <Widget>[
+                  Text('Akun'),
+                ]),
+          )),
+    ));
   }
 }
